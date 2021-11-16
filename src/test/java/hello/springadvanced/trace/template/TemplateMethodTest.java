7007ff5ff38e36ac4652e0d8097b5bc8923594e4
이경진 <bramhome5848@gmail.com>
2021-11-16 21:03:28 +0900

    20211116 템플릿 메서드 패턴과 콜백 패턴 - 템플릿 메서드 패턴 - 예제3

--- src/test/java/hello/springadvanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/springadvanced/trace/template/TemplateMethodTest.java
@@ -51,4 +51,35 @@ public class TemplateMethodTest {
         AbstractTemplate template2 = new SubClassLogic2();
         template2.execute();
     }
+
+    /**
+     * 템플릿 메서드 패턴,익명 내부 클래스 사용
+     * 템플릿 메서드 패턴은 SubClassLogic1 , SubClassLogic2 처럼 클래스를 계속 만들어야 하는 단점
+     * 익명 내부 클래스를 사용하면 객체 인스턴스를 생성하면서 동시에 생성할 클래스를 상속 받은 자식 클래스를 정의
+     * 이 클래스는 SubClassLogic1 처럼 직접 지정하는 이름이 없고 클래스 내부에 선언되는 클래스여서 익명 내부 클래스
+     */
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+
+        //자바가 임의로 만들어주는 익명 내부 클래스 이름은 TemplateMethodTest$1
+        log.info("클래스 이름1={}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+
+        //자바가 임의로 만들어주는 익명 내부 클래스 이름은 TemplateMethodTest$2
+        log.info("클래스 이름2={}", template2.getClass());
+        template2.execute();
+    }
 }
