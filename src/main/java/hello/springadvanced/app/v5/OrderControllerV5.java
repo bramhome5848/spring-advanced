e0586a1f29978ac725b2208fa36b3443fccbbe39
이경진 <bramhome5848@gmail.com>
2021-11-22 17:48:29 +0900

    20211122 템플릿 메서드 패턴과 콜백 패턴 - 템플릿 콜백 패턴 - 정리

--- src/main/java/hello/springadvanced/app/v5/OrderControllerV5.java
+++ src/main/java/hello/springadvanced/app/v5/OrderControllerV5.java
@@ -7,6 +7,12 @@ import hello.springadvanced.trace.template.AbstractTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 한계
+ * 지금까지 방식의 한계는 아무리 최적화를 해도 결국 로그 추적기를 적용하기 위해서 원본 코드의 수정이 필요
+ * 클래스가 수백개이면 수백개를 더 힘들게 수정하는가 조금 덜 힘들게 수정하는가의 차이가 있을 뿐, 본질적으로 코드를 다 수정해야 하는 것은 마찬가지
+ * 지금부터 원본 코드를 손대지 않고 로그 추적기를 적용할 수 있는 방법 -> 프록시에 대한 이해가 필요
+ */
 @RestController
 public class OrderControllerV5 {
 
